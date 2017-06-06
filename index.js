b13b9c381d40980f594f0ba740397d8ddab51a44
Tom <tomtaoran@aliyun.com>
2017-06-06 00:31:41 -0400

    Complete Homework

--- index.js
+++ index.js
@@ -5,7 +5,38 @@ function handleSubmit(ev){
     ev.preventDefault() //just to stop the default refresh
     const f= ev.target
     const heading = document.querySelector('h1')
-    heading.textContent = f.personName.value
+    heading.textContent = f.personName.value + ' is now age ' + f.personAge.value
 
 }
+
+function reverseString(ev) {
+    ev.preventDefault()
+    const heading = document.querySelector('h1')
+    const str= heading.textContent
+    const paragraph = document.querySelector('.reverse')
+
+    const splitString = str.split(""); 
+
+    const reverseArray = splitString.reverse(); 
+    
+    const joinArray = reverseArray.join("");
+
+    paragraph.textContent = joinArray
+}
+
+function changeAppearance(ev){
+    ev.preventDefault()
+    const f= ev.target
+    const heading = document.querySelector('h1')
+    const str= heading.textContent
+    if(f.personAge.value>=75){
+        heading.style.color = "red";
+    }else{
+        heading.style.color = "blue";
+    }
+    //We change the heading's color by age.
+    //Side Note: If you are young (in my belief), color is blue, otherwise, color is red.
+}
 personForm.addEventListener('submit', handleSubmit) //Do NOT do handleSubmit(ev) here. otherwise we are only using the return value!!!
+personForm.addEventListener('submit', reverseString)
+personForm.addEventListener('submit', changeAppearance)
